d2e6f6e1b12356a69f59514ad2ef7937bffea854
Muhammad Fathy Moheb <mohamed192509@fci.bu.edu.eg>
2025-04-29 17:56:07 +0300

    Add the main file.py Add Archive folder for recording the progress write the first class and some functions

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# geometric-shapes-Calculator-
+# geometric-shapes-Calculator
+
+A program for calculating the Perimeter and area of choosen geometric shapes
---
+++ geometric_cal.py
@@ -0,0 +1,39 @@
+class RegularShapes:
+    
+    def __init__(self,perimeter,area,sides,side_value):
+        self.sides = sides
+        self.perimeter = perimeter
+        self.area = area
+        self.side_value = side_value
+    
+    def take_user_input(self):
+        while True:
+            try:
+                choice = int(input("Choose a shape: "))
+                if choice <= 0:
+                    print("No such choice! Try again.")
+                else:
+                    return choice
+            except ValueError:
+                print("Invalid input! Numeric input only accepted.")
+
+    def display_choicess(self):
+        shapes = {
+            1: 'Square',
+            2: 'Rectangle',
+            3: 'Triangle',
+            4: 'Circle',
+            5: 'Other'
+        }
+        for key, value in shapes.items():
+            print(f"[{key}]: {value}")
+    
+    def square(self):
+        print(f"### -- Square: A quadrilateral with four equal sides and four right angles.".title())
+        self.perimeter = self.sides*4
+        self.area = self.sides*self.side_value
+
+
+test = Calculator()
+test.display_shapes()
+print(test.take_user_input())
